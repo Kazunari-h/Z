69a5d0b05ca2ff925f61b62e8b61de79d9ebad2e
Kazunari Hirosawa <9694038+Kazunari-h@users.noreply.github.com>
2024-06-11 13:51:33 +0900

    Initial Commit

---
+++ Z/ZApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZApp.swift
+//  Z
+//
+//  Created by Kazunari Hirosawa on 2024/06/11.
+//
+
+import SwiftUI
+
+@main
+struct ZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
