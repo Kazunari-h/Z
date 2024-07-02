4d15ff822f08218922907887c44a90723c90fd67
Kazunari Hirosawa <9694038+Kazunari-h@users.noreply.github.com>
2024-07-02 18:48:45 +0900

    いいね関連のファイルの追加

---
+++ Z/View/FavoriteView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteView.swift
+//  Z
+//
+//  Created by Kazunari Hirosawa on 2024/06/18.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text("FavoriteView")
+    }
+}
+
+#Preview {
+    FavoriteView()
+}
