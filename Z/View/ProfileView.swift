38d3ed7ecdb8738096a297f2d31d4d8c4556ee15
Kazunari Hirosawa <9694038+Kazunari-h@users.noreply.github.com>
2024-07-02 18:49:02 +0900

    プロフィール関連のファイルの追加

---
+++ Z/View/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Z
+//
+//  Created by Kazunari Hirosawa on 2024/06/18.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("ProfileView")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
