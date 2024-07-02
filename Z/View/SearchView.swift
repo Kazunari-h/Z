3fb7373a5bba068db4c0bace6b66dd865e79f090
Kazunari Hirosawa <9694038+Kazunari-h@users.noreply.github.com>
2024-07-02 18:49:12 +0900

    検索画面を追加

---
+++ Z/View/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Z
+//
+//  Created by Kazunari Hirosawa on 2024/06/18.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("SearchView")
+    }
+}
+
+#Preview {
+    SearchView()
+}
