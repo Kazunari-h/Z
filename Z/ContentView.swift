c56a6c56c88bb34372264310124b64e0041cf18d
Kazunari Hirosawa <9694038+Kazunari-h@users.noreply.github.com>
2024-07-02 18:50:29 +0900

    feat: FirebaseのセットアップとFirebase設定のためのAppDelegateを追加

--- Z/ContentView.swift
+++ Z/ContentView.swift
@@ -7,18 +7,72 @@
 
 import SwiftUI
 
+// struct 構造体の名前: 継承するものの名前 {}
+// 継承するものの名前 => プロトコル (or struct)
+// プロトコル(or struct)は省略可能
+
 struct ContentView: View {
+
+    // ViewはBodyで構成される
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+                NavigationView {
+                    TimelineView()
+                        .navigationBarTitleDisplayMode(.inline)
+                        .navigationTitle("タイムライン")
+                        .navigationBarItems(trailing: Button(action: {}, label: {
+                            Image(systemName: "bell.badge.fill")
+                                .foregroundStyle(.red, .yellow)
+                        }))
+                }
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("タイムライン")
+                }
+                .foregroundStyle(.green, .green)
+
+            SearchView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("検索")
+                }
+                .foregroundStyle(.blue, .blue)
+
+
+            FavoriteView()
+                .tabItem {
+                    Image(systemName: "heart")
+                    Text("いいね")
+                }
+                .foregroundStyle(.pink, .pink)
+
+
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("プロフィール")
+                }
+                .foregroundStyle(.orange, .orange)
+
+
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+//         VStack { // 縦並びにするView (Flexbox縦のdiv)
+// //            Image(systemName: "globe") 
+// //            // 引数のラベルによって意味が違ってくる
+// //                .imageScale(.large) // 画像のサイズを変更
+// //                .foregroundStyle(.tint) // 画像の色のつき方を指定
+// //                // .foregroundColor(.red) // Viewの色を指定
+// //            Text("Hello, world!")
+// //                .foregroundColor(.red) // Viewの色を指定
+            
+//             Text("こんにちは")
+//         }
+//         .padding()
--- Z/ZApp.swift
+++ Z/ZApp.swift
@@ -5,10 +5,31 @@
 //  Created by Kazunari Hirosawa on 2024/06/11.
 //
 
+
+// SwiftUIとは？
+// Swiftを使ったAppleが作ったUIフレームワーク
+// JSのReactやVueのようなもの
+
+// 構造体(struct)でAppやViewを定義する！
+
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
 
 @main
 struct ZApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
